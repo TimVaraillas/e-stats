29e905961e4b6e0e9ede0ca67ba9fdae1f07f69e
Tim <timothee.varaillas@gmail.com>
2022-03-24 17:12:25 +0100

    + Configuration initiale de l'application

--- src/main.js
+++ src/main.js
@@ -1,6 +1,34 @@
 import { createApp } from 'vue';
-import App from './App.vue';
+
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+import 'normalize.css/normalize.css';
+
+import ElementPlus from 'element-plus';
+import fr from 'element-plus/es/locale/lang/fr';
+// import 'element-plus/theme-chalk/index.css';
+import './theme/index.css';
+
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+import App from './App.vue';
+
+library.add(fas);
+library.add(fab);
+library.add(far);
+dom.watch();
+
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+app.use(VueAxios, axios);
+app.use(ElementPlus, { locale: fr });
+app.mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "@/styles/global.scss";`
+      },
+    }
+  },
   transpileDependencies: true,
 });
