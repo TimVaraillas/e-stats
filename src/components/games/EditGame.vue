b741679bb44df0edde9fb1bb7e186b763674b31c
Tim <timothee.varaillas@gmail.com>
2022-03-29 13:29:46 +0200

    + Création du composant EditGame

---
+++ src/components/games/EditGame.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="edit-game">
+
+    <n-form :model="game">
+      <n-grid span="24" x-gap="20">
+
+        <n-form-item-gi span="24" label="Lieu" path="place">
+          <n-input v-model:value="game.place" type="text"/>
+        </n-form-item-gi>
+
+        <n-form-item-gi span="24" label="Date et heure du match" path="datetime">
+          <n-date-picker
+            v-model:value="game.datetime"
+            type="datetime"
+            :time-picker-props="{ minutes: 10, seconds: [0] }"
+            clearable />
+        </n-form-item-gi>
+
+        <n-form-item-gi span="16" label="Équipe locale" path="localeTeam.name">
+          <n-input v-model:value="game.localeTeam.name" type="text"/>
+        </n-form-item-gi>
+
+        <n-form-item-gi span="8" label="Couleur" path="localeTeam.color">
+          <n-color-picker v-model:value="game.localeTeam.color" :show-alpha="false" />
+        </n-form-item-gi>
+
+        <n-form-item-gi span="16" label="Équipe visiteuse" path="awayTeam.name">
+          <n-input v-model:value="game.awayTeam.name" type="text"/>
+        </n-form-item-gi>
+
+        <n-form-item-gi span="8" label="Couleur" path="awayTeam.color">
+          <n-color-picker v-model:value="game.awayTeam.color" :show-alpha="false" />
+        </n-form-item-gi>
+
+      </n-grid>
+    </n-form>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+import moment from 'moment';
+import {
+  NGrid,
+  NForm,
+  NFormItemGi,
+  NInput,
+  NDatePicker,
+  NColorPicker,
+} from 'naive-ui';
+
+export default {
+  name: 'AddGameView',
+  components: {
+    NGrid,
+    NForm,
+    NFormItemGi,
+    NInput,
+    NDatePicker,
+    NColorPicker,
+  },
+  data() {
+    return {
+      game: {
+        place: '',
+        datetime: this.getDefaultDatetime(),
+        localeTeam: {
+          name: '',
+          color: '#FFCC33',
+        },
+        awayTeam: {
+          name: '',
+          color: '#FF6680',
+        },
+      },
+    };
+  },
+  methods: {
+    ...mapActions('games', [
+      'addGame',
+    ]),
+
+    getDefaultDatetime() {
+      let start = moment();
+      start = start.endOf('hour');
+      start = start.set({ minute: 0, second: 0, millisecond: 0 });
+      console.log(start.format('LLLL'));
+      return start.toDate();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.edit-game {
+  .n-date-picker {
+    flex: 1;
+  }
+  .actions {
+    display: flex;
+    justify-content: flex-end;
+  }
+}
+</style>
