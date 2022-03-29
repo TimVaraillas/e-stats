4f99793710dfa13fb3fdd46107c7ea6e2de6628e
Tim <timothee.varaillas@gmail.com>
2022-03-29 13:27:01 +0200

    @ Remplacement du framework de design element-plus par naive-ui

--- src/main.js
+++ src/main.js
@@ -4,11 +4,6 @@ import axios from 'axios';
 
 import 'normalize.css/normalize.css';
 
-import ElementPlus from 'element-plus';
-import fr from 'element-plus/es/locale/lang/fr';
-// import 'element-plus/theme-chalk/index.css';
-import './theme/index.css';
-
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
@@ -32,5 +27,4 @@ store.$axios = axios;
 
 app.use(store);
 app.use(router);
-app.use(ElementPlus, { locale: fr });
 app.mount('#app');
