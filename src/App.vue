7d2116ba7844f57a71bad710efedd41c5b8db454
Tim <timothee.varaillas@gmail.com>
2022-03-30 14:56:32 +0200

    @ [app] Amélioration du layout global

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
   <n-config-provider :theme="darkTheme" :locale="frFR" :date-locale="dateFrFR">
-    <n-layout style="height: 100vh">
-      <n-layout-header style="height: 64px; padding: 24px" bordered>
-        <b>E-stats</b>
-      </n-layout-header>
-      <n-layout position="absolute" style="top: 64px; bottom: 64px" has-sider>
-        <n-layout-sider :native-scrollbar="false" bordered>
-          <n-menu ref="menu" :options="options" v-model="selectedKey"/>
-        </n-layout-sider>
-        <n-layout :native-scrollbar="false">
-          <router-view />
+    <n-message-provider placement="bottom" :keep-alive-on-hover="true">
+      <n-layout style="height: 100vh">
+        <n-layout-header class="topbar" bordered>
+          <img class="logo" src="@/assets/logo/logo-colors.svg" alt="E-Stats">
+        </n-layout-header>
+        <n-layout position="absolute" style="top: 64px; bottom: 64px" has-sider>
+          <n-layout-sider
+            bordered
+            collapse-mode="width"
+            show-trigger="arrow-circle"
+            :native-scrollbar="false">
+            <n-menu ref="menu" :options="options" v-model="selectedKey" />
+          </n-layout-sider>
+          <n-layout :native-scrollbar="false">
+            <router-view />
+          </n-layout>
         </n-layout>
+        <n-layout-footer position="absolute" style="height: 64px; padding: 24px" bordered>
+          <span>© {{ new Date().getFullYear() }}, <b>E-Stats</b></span>
+        </n-layout-footer>
       </n-layout>
-      <n-layout-footer
-        position="absolute"
-        style="height: 64px; padding: 24px"
-        bordered
-      >
-        <span>© {{ new Date().getFullYear() }}, <b>E-Stats</b></span>
-      </n-layout-footer>
-    </n-layout>
-
+    </n-message-provider>
   </n-config-provider>
 </template>
 
@@ -35,6 +36,7 @@ import {
   NLayoutFooter,
   NLayoutSider,
   NMenu,
+  NMessageProvider,
 } from 'naive-ui';
 import { h } from 'vue';
 import { RouterLink } from 'vue-router';
@@ -64,6 +66,7 @@ export default {
     NLayoutFooter,
     NLayoutSider,
     NMenu,
+    NMessageProvider,
   },
   setup() {
     return {
@@ -83,7 +86,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-::v-deep .menu-icon {
-  font-size: 13px;
+.topbar {
+  height: 64px;
+  padding: 0 24px;
+  display: flex;
+  align-items: center;
+
+  .logo {
+    height: 40px;
+  }
+}
+::v-deep {
+  .n-menu {
+    .menu-icon {
+      font-size: 14px;
+    }
+    &.n-menu--collapsed {
+      .menu-icon {
+        font-size: 18px;
+      }
+    }
+  }
 }
 </style>
