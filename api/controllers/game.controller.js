100d3e78d094c3e95c650a8b37d093a8da8f659b
Tim <timothee.varaillas@gmail.com>
2022-03-30 14:53:41 +0200

    + [API] Ajout de la route updatebyId

--- api/controllers/game.controller.js
+++ api/controllers/game.controller.js
@@ -1,4 +1,5 @@
 const Game = require("../models/game.model.js");
+const _omit = require("lodash/omit")
 
 exports.getAll = (req, res) => {
   Game.find()
@@ -32,7 +33,7 @@ exports.post = (req, res) => {
   const game = new Game(req.body);
 
   game.save()
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
     .catch(err => {
@@ -44,17 +45,32 @@ exports.post = (req, res) => {
     });
 };
 
-exports.deleteById = (req, res) => {
-  
-  Game.deleteOne({ _id: req.params.id })
-    .then(() => {
-      res.send({deleted: req.params.id });
+exports.updateById = (req, res) => {
+  Game
+    .findByIdAndUpdate(req.body.id, { $set: { ..._omit(req.body, ["id"]) } }, { returnDocument: "after" })
+    .then((data) => {
+      res.send(data);
     })
     .catch(err => {
       console.log(err);
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the game."
+          err.message || "Some error occurred while updating the game."
+      });
+    });
+};
+
+exports.deleteById = (req, res) => {
+  
+  Game.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.send({ deleted: req.params.id });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while deleting the game."
       });
     });
 };
--- api/routes/game.routes.js
+++ api/routes/game.routes.js
@@ -5,6 +5,7 @@ const gameController = require("../controllers/game.controller.js");
 router.get("/", gameController.getAll);
 router.get("/:id", gameController.getById);
 router.post("/", gameController.post);
+router.put("/:id", gameController.updateById);
 router.delete("/:id", gameController.deleteById);
 
 module.exports = router;
